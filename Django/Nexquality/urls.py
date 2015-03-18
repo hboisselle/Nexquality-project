62cd8a7ca79a92306559c4e3c008e47b72bf77dc
hboisselle <hugo.boisselle@gmail.com>
2015-03-17 20:38:26 -0400

    46.UserLogin
    Created user login view

--- Django/Nexquality/urls.py
+++ Django/Nexquality/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import patterns, url
-
+from django.conf.urls import patterns, url, include
 from Nexquality import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.login_user, name='login_user')
+    url(r'^', include('django.contrib.auth.urls')),
+    url(r'^accounts/profile/$', views.user_profile, name='user_profile'),
 )
--- Django/Nexquality/views.py
+++ Django/Nexquality/views.py
@@ -1,26 +1,11 @@
 from django.shortcuts import render_to_response
 from django.contrib.auth import authenticate, login
-from django.core.context_processors import csrf
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
-def login_user(request):
-    c = {}
-    c.update(csrf(request))
-    state = "Please log in below..."
-    username = password = ''
-    if request.POST:
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            if user.is_active:
-                login(request, user)
-                state = "You're successfully logged in!"
-            else:
-                state = "Your account is not active,"
-                state += " please contact the site admin."
-        else:
-            state = "Your username and/or password were incorrect."
-
-    return render_to_response('Nexquality/login.html', {'csrf_token': c, 'state': state, 'username': username})
+@login_required
+def user_profile(request):
+    return render(request, "accounts/profile.html")
