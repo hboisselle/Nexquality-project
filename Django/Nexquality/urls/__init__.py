0c42b2da99ab2b4c6aacb542dea511430d500e59
hboisselle <hugo.boisselle@gmail.com>
2015-04-06 18:19:59 -0400

    FileandfolderRefactor

--- Django/Nexquality/urls/__init__.py
+++ Django/Nexquality/urls/__init__.py
@@ -1,10 +1,10 @@
 from django.conf.urls import patterns, url, include
-from Nexquality.views import index
+from Nexquality.views import index, profile
 
 
 urlpatterns = patterns('',
     url(r'^$', index.index, name='index'),
-    url(r'^profile/', include('Nexquality.urls.profile', namespace="profile")),
+    url(r'^profile/$', profile.user_profile, name='profile'),
     url(r'^project/', include('Nexquality.urls.project', namespace='project')),
     url(r'^registration/', include('Nexquality.urls.registration', namespace="registration")),
     url(r'^data-importation/', include('Nexquality.urls.data_import', namespace="data_importation")),
--- Django/Nexquality/urls/profile.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, url
-from Nexquality.views import profile
-
-
-urlpatterns = patterns('',
-    url(r'^profile/$', profile.user_profile, name='user_profile'),
-)
